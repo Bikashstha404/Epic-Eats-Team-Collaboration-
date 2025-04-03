7f2855e6c6ceda65129e84721148ee3ac359b58a
Winendra Basukala <np03cs4a230049@heraldcollege.edu.np>
2025-04-03 17:41:55 +0545

    Added Admin Manage Resturant for approvel

--- app/Http/Controllers/Admin/ManageController.php
+++ app/Http/Controllers/Admin/ManageController.php
@@ -166,6 +166,28 @@
 
     }
     // End Method 
+
+        ///////// FOR ALL Pending and Approve Restaurant method 
+ 
+    public function PendingRestaurant(){
+        $client = Client::where('status',0)->get();
+        return view('admin.backend.restaurant.pending_restaurant',compact('client')); 
+    }
+     // End Method 
+
+     public function ClientChangeStatus(Request $request){
+        $client = Client::find($request->client_id);
+        $client->status = $request->status;
+        $client->save();
+        return response()->json(['success' => 'Status Change Successfully']);
+    }
+     // End Method 
+    
+     public function ApproveRestaurant(){
+        $client = Client::where('status',1)->get();
+        return view('admin.backend.restaurant.approve_restaurant',compact('client')); 
+    }
+    // End Method 
  
  
  } 
