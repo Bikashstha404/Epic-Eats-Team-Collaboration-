53c14eef1d487169fe02fb343dbd28bf2125d7e3
Safal kc <np03cs4a230047@heraldcollege.edu.np>
2025-04-05 10:28:35 +0545

    enabled restaurant detail view from homepage click

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Client\RestaurantController;
 use App\Http\Controllers\Admin\ManageController;
+use App\Http\Controllers\Frontend\HomeController;
 
 // Route::get('/', function () {
 //     return view('welcome');
@@ -140,3 +141,10 @@ Route::middleware(['client','status'])->group(function () {
 
   /// That will be for all user 
   Route::get('/changeStatus', [RestaurantController::class, 'ChangeStatus']);
+
+
+
+  Route::controller(HomeController::class)->group(function(){
+    Route::get('/restaurant/details/{id}', 'RestaurantDetails')->name('res.details'); 
+    
+});
