c31b54919bd3781af4bdce3c33b719f53ebe337b
Winendra Basukala <np03cs4a230049@heraldcollege.edu.np>
2025-05-07 07:04:59 +0545

    implemented add to wishlist

--- resources/views/frontend/dashboard/sidebar.blade.php
+++ resources/views/frontend/dashboard/sidebar.blade.php
@@ -101,6 +101,11 @@ $profileData = App\Models\User::find($id);
          </li>
 
          <li class="nav-item">
+             <a class="nav-link {{ Route::currentRouteName() === 'all.wishlist' ? 'active' : '' }}"  href="{{ route('all.wishlist') }}" role="tab" aria-controls="orders" aria-selected="true"><i class="icofont-food-cart"></i> Favourites </a>
+          </li>
+ 
+
+         <li class="nav-item">
             <a class="nav-link {{ Route::currentRouteName() === 'user.order.list' ? 'active' : '' }}"
                href="{{ route('user.order.list') }}">
                <i class="icofont-basket"></i> Orders
