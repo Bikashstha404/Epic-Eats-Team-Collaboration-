e839d9769bb15957bd85ba1f9b1b51ef1656f914
Winendra Basukala <np03cs4a230049@heraldcollege.edu.np>
2025-05-11 14:21:38 +0545

    User: add or remove food

--- resources/views/client/index.blade.php
+++ resources/views/client/index.blade.php
@@ -1,14 +1,23 @@
 @extends('client.client_dashboard')
 @section('client')
 
-@php
-$id = Auth::guard('client')->id();
-$client = App\Models\Client::find($id);
-$status = $client->status;
-@endphp
+    @php
+        $id = Auth::guard('client')->id();
+        $client = App\Models\Client::find($id);
+        $status = $client->status;
+    @endphp
 
-<div class="page-content">
-    <h1><b><u>Welcome to Owner's Dashboard</u></b></h1>
-</div>
+    <div class="page-content">
+        <h1 class="mb-4"><b><u>Welcome to Owner's Dashboard</u></b></h1>
+
+            @if ($status === '1')
+        <h4>Restaunt Account is <span class="text-success">Active</span> </h4>
+    @else
+        <h4>Restaurant Account is <span class="text-danger">InActive</span> </h4>
+        <p class="text-danger"><b>Plz wait admin will check and approve your account </b> </p>
+    @endif
+    </div>
+
+
 
 @endsection
